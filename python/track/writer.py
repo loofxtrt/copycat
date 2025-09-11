6389866225d5b2b2420bf07be8248626e0679129
loo <loofxtrt@gmail.com>
2025-09-11 20:45:01 -0300

    track

--- python/track/writer.py
+++ python/track/writer.py
@@ -1,44 +1,63 @@
-from xml.dom import minidom
 from pathlib import Path
 
-from yattag import Doc
+from pytablewriter import MarkdownTableWriter
 
-import contents
+from contents import default_apps, default_places
 
-def create_table(rows: list[dict]):
-    doc, tag, text = Doc().tagtext()
+def resolve_table_writing(content_dict: dict):
+    group_name = content_dict.get("group_name")
+    origin_dir = content_dict.get("origin_dir")
+    icon_names = content_dict.get("icon_names")
 
-    # criar a tabela com borda, largura etc.
-    with tag("table", border="1", width="100%"):
-        # cabeçalho
-        with tag("tr"):
-            for header in ["Icon", "Source", "Changes"]:
-                with tag("th"): text(header)
-        
-        # criar um table row pra cada entrada do array de dicts
-        for row in rows:
-            with tag("tr"):
-                # escrever cada valor do dicionário como um table data
-                icon_name = row["icon_name"]
-                if not icon_name.endswith(".svg"):
-                    icon_name += ".svg"
+    rows = []
+    for i_name in icon_names:
+        if not i_name.endswith(".svg"):
+            print(f"{i_name} é inválido por não terminar em .svgs")
+            continue
 
-                origin_dir = row["origin_dir"]
+        # extrair os dados da entrada do dicionário de conteúdos
+        i_path = Path("./copycat/", origin_dir, i_name)
+        icon = f"![icon]({i_path})"
+        full_path = f"`{i_path}`"
+        file_name = f"`{i_name}`"
+        norm_origin_dir = f"`{origin_dir}`"
 
-                full_path = Path(origin_dir, icon_name)
+        # adicionar verificação se o ícone é ou não um symlink
+        # se ele NÃO for, ganha a checkmark verde, pra indicar que é real
+        # ícones de: lucide icons
+        resolved_path = Path(__file__).resolve().parent.parent.parent / "copycat" / origin_dir / i_name
+        checkmark_width = 15
+        if not resolved_path.is_symlink():
+            is_symlink = f'<img src="./python/track/check.svg" width={checkmark_width}>'
+        else:
+            is_symlink = f'<img src="./python/track/ban.svg" width={checkmark_width}>'
 
-                with tag("td"):
-                    # imagem do ícone
-                    doc.stag("img", src=f"{origin_dir}/{icon_name}.svg", width="24")
-                with tag("td"):
-                    text(f" {icon_name}")
-                with tag("td"):
-                    text(origin_dir)
-                with tag("td"):
-                    text(full_path)
+            # deixar os textos do row em itálico caso seja um symlink
+            full_path = f"<i>{full_path}</i>"
+            file_name = f"<i>{file_name}</i>"
+            norm_origin_dir = f"<i>{norm_origin_dir}</i>"
 
-    # renderizar a tabela e retornar
-    raw_html = doc.getvalue()
-    pretty_html = minidom.parseString(raw_html).toprettyxml()
-    
-    return pretty_html
+        # adicionar +1 row ao array final
+        rows.append(
+            [icon, is_symlink, full_path, file_name, norm_origin_dir]
+        )
+
+    # gerar a tabela markdown contendo os rows obtidos
+    # e escrever ela como uma string com .dumps
+    writer = MarkdownTableWriter(
+        table_name=group_name,
+        headers=["Icon", "Is real", "Full path", "File name", "Origin directory"],
+        value_matrix=rows
+    )
+
+    string_table = writer.dumps()
+    return string_table
+
+table_default_apps = resolve_table_writing(default_apps)
+table_default_places = resolve_table_writing(default_places)
+
+condensed = table_default_apps + table_default_places
+
+# escrever o resultado final no arquivo markdown
+with open("../../TRACK.md", "w") as f:
+    f.write(condensed)
