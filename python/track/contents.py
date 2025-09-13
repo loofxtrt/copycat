9833a52ee4acc657f2532ae2ca1d3642dcfd9040
loo <loofxtrt@gmail.com>
2025-09-13 06:55:21 -0300

    etc

--- python/track/contents.py
+++ python/track/contents.py
@@ -1,145 +1,192 @@
-apps = "apps/scalable/"
-places = "places/scalable"
+apps = 'apps/scalable/'
+places = 'places/scalable'
+mimetypes = 'mimetypes/scalable'
 
-# --- apps --------------------------------------------------
+# as tuplas devem seguir o padrão (origem, nome do ícone)
+
+# --- apps (com algumas exceções) --------------------------------------------------
 plain_folder_group = {
-    "group_name": "Folder-likes - Plain",
-    "copies": [
-        (apps, "d3lphin.svg"),
-        (apps, "org.kde.plasma.folder.svg"),
-        (apps, "thunar-filemanager.svg"),
-        (apps, "xfce-filemanager.svg"),
-        (apps, "file-manager-blue.svg"),
-        (apps, "com.system76.CosmicFiles.svg"),
-        (apps, "file-manager.svg"),
-        (apps, "folder_doc_q4os_startmenu.svg"),
-        (apps, "org.kde.dolphin.svg"),
-        (apps, "spacefm-blue.svg"),
-        (apps, "dde-file-manager.svg"),
-        (apps, "dolphin.svg"),
-        (apps, "gnome-commander.svg"),
-        (apps, "gnome-encfs-manager.svg"),
-        (apps, "kfm.svg"),
-        (apps, "nautilus.svg"),
-        (apps, "nemo.svg"),
-        (apps, "redhat-filemanager.svg"),
-        (apps, "system-file-manager.svg"),
-        (apps, "thunar.svg"),
-        (apps, "Thunar.svg"),
-        (apps, "user-file-manager.svg"),
-        (apps, "mc.svg"),
-        (apps, "MidnightCommander.svg"),
-        (apps, "mucommander.svg"),
-        (apps, "nautilus-actions-config-tool.svg"),
-        (apps, "nautilus-actions.svg"),
-        (apps, "org.gnome.files.svg"),
-        (apps, "org.gnome.Files.svg"),
-        (apps, "org.gnome.nautilus.svg"),
-        (apps, "org.gnome.Nautilus.svg"),
-        (apps, "org.kde.index.svg"),
-        (apps, "org.xfce.filemanager.svg"),
-        (apps, "org.xfce.panel.directorymenu.svg"),
-        (apps, "org.xfce.thunar.svg"),
-        (apps, "pcmanfm-qt.svg"),
-        (apps, "spacefm.svg"),
-        (apps, "fma-config-tool.svg"),
-        (apps, "caja-actions.svg"),
-        (places, "folder.svg"),
-        (places, "folder_2.svg"),
-        (places, "gnome-folder.svg"),
-        (places, "gnome-fs-directory-accept.svg"),
-        (places, "gnome-fs-directory.svg"),
-        (places, "gtk-directory.svg"),
-        (places, "inode-directory.svg"),
-        (places, "stock_folder.svg"),
+    'group_name': 'Folder-likes - Plain',
+    'copies': [
+        (apps, 'd3lphin'),
+        (apps, 'org.kde.plasma.folder'),
+        (apps, 'thunar-filemanager'),
+        (apps, 'xfce-filemanager'),
+        (apps, 'file-manager-blue'),
+        (apps, 'com.system76.CosmicFiles'),
+        (apps, 'file-manager'),
+        (apps, 'folder_doc_q4os_startmenu'),
+        (apps, 'org.kde.dolphin'),
+        (apps, 'spacefm-blue'),
+        (apps, 'dde-file-manager'),
+        (apps, 'dolphin'),
+        (apps, 'gnome-commander'),
+        (apps, 'gnome-encfs-manager'),
+        (apps, 'kfm'),
+        (apps, 'nautilus'),
+        (apps, 'nemo'),
+        (apps, 'redhat-filemanager'),
+        (apps, 'system-file-manager'),
+        (apps, 'thunar'),
+        (apps, 'Thunar'),
+        (apps, 'user-file-manager'),
+        (apps, 'mc'),
+        (apps, 'MidnightCommander'),
+        (apps, 'mucommander'),
+        (apps, 'nautilus-actions-config-tool'),
+        (apps, 'nautilus-actions'),
+        (apps, 'org.gnome.files'),
+        (apps, 'org.gnome.Files'),
+        (apps, 'org.gnome.nautilus'),
+        (apps, 'org.gnome.Nautilus'),
+        (apps, 'org.kde.index'),
+        (apps, 'org.xfce.filemanager'),
+        (apps, 'org.xfce.panel.directorymenu'),
+        (apps, 'org.xfce.thunar'),
+        (apps, 'pcmanfm-qt'),
+        (apps, 'spacefm'),
+        (apps, 'fma-config-tool'),
+        (apps, 'caja-actions'),
+        (places, 'folder'),
+        (places, 'folder_2'),
+        (places, 'gnome-folder'),
+        (places, 'gnome-fs-directory-accept'),
+        (places, 'gnome-fs-directory'),
+        (places, 'gtk-directory'),
+        (places, 'inode-directory'),
+        (places, 'stock_folder'),
+        (mimetypes, 'inode-directory')
     ]
 }
 
-krusader_group = {
-    "group_name": "Folder-likes - Krusader",
-    "copies": [
-        (apps, "krusader_blue.svg"),
-        (apps, "krusader_user.svg"),
+variations_group = {
+    'group_name': 'Folder-likes - Variations',
+    'copies': [
+        (apps, 'AirPortUtility'),
+        (apps, 'athena'),
+        (apps, 'cs-network'),
+        (apps, 'gnome-network-properties'),
+        (apps, 'kali-wireless-attacks-trans'),
+        (apps, 'kfoldersync'),
+        (apps, 'krusader_blue'),
+        (apps, 'krusader_red'),
+        (apps, 'krusader_root'),
+        (apps, 'krusader_user'),
+        (apps, 'linssid'),
+        (apps, 'mate-netspeed-applet'),
+        (apps, 'mate-network-properties'),
+        (apps, 'nautilus-alt'),
+        (apps, 'network-manager'),
+        (apps, 'network-workgroup'),
+        (apps, 'networkmanager'),
+        (apps, 'org.kde.plasma.vault'),
+        (apps, 'plasmavault'),
+        (apps, 'wicd'),
+        (apps, 'wicd-gtk'),
+        (apps, 'wifi-radar'),
+        (apps, 'winefile'),
+    ]
+}
+
+derivations_group = {
+    'group_name': 'Folder-likes - Derivations',
+    'copies': [
+        (apps, 'file-manager-green'),
+        (apps, 'file-manager-red'),
+        (apps, 'spacefm-green'),
+        (apps, 'spacefm-red'),
+        (apps, 'system-file-manager-root'),
+        (places, 'folder-black'),
+        (places, 'folder-blue'),
+        (places, 'folder-brown'),
+        (places, 'folder-cyan'),
+        (places, 'folder-green'),
+        (places, 'folder-grey'),
+        (places, 'folder-magenta'),
+        (places, 'folder-orange'),
+        (places, 'folder-red'),
+        (places, 'folder-violet'),
+        (places, 'folder-yellow'),
     ]
 }
 
 # --- places --------------------------------------------------
 folder_locked_group = {
-    "group_name": "Folder-likes - Locked",
-    "copies": [
-        (places, "folder-locked.svg"),
-        (places, "certificate-server.svg"),
-        (places, "folder-ecrypted.svg"),
-        (places, "folder-encrypted.svg"),
+    'group_name': 'Folder-likes - Locked',
+    'copies': [
+        (places, 'folder-locked'),
+        (places, 'certificate-server'),
+        (places, 'folder-ecrypted'),
+        (places, 'folder-encrypted'),
     ]
 }
 
 folder_favorites_group = {
-    "group_name": "Folder-likes - Favorites",
-    "copies": [
-        (places, "folder-favorites.svg"),
-        (places, "favorites.svg"),
-        (places, "user-bookmarks.svg"),
-        (places, "folder-bookmarks.svg"),
-        (places, "folder-bookmark.svg"),
-        (places, "gnome-fs-bookmark.svg"),
-        (places, "stock_bookmark.svg"),
+    'group_name': 'Folder-likes - Favorites',
+    'copies': [
+        (places, 'folder-favorites'),
+        (places, 'favorites'),
+        (places, 'user-bookmarks'),
+        (places, 'folder-bookmarks'),
+        (places, 'folder-bookmark'),
+        (places, 'gnome-fs-bookmark'),
+        (places, 'stock_bookmark'),
     ]
 }
 
 folder_download_group = {
-    "group_name": "Folder-likes - Downloads",
-    "copies": [
-        (places, "folder-download-open.svg"),
-        (places, "folder-downloads.svg"),
-        (places, "folder-download.svg"),
+    'group_name': 'Folder-likes - Downloads',
+    'copies': [
+        (places, 'folder-download-open'),
+        (places, 'folder-downloads'),
+        (places, 'folder-download'),
     ]
 }
 
 folder_image_group = {
-    "group_name": "Folder-likes - Pictures",
-    "copies": [
-        (places, "folder-pictures.svg"),
-        (places, "folder_pictures.svg"),
-        (places, "folder-picture.svg"),
-        (places, "user-images.svg"),
-        (places, "user-pictures.svg"),
+    'group_name': 'Folder-likes - Pictures',
+    'copies': [
+        (places, 'folder-pictures'),
+        (places, 'folder_pictures'),
+        (places, 'folder-picture'),
+        (places, 'user-images'),
+        (places, 'user-pictures'),
     ]
 }
 
 folder_remote_group = {
-    "group_name": "Folder-likes - Remote",
-    "copies": [
-        (places, "folder-remote-apf.svg"),
-        (places, "folder-remote-ftp.svg"),
-        (places, "folder-remote-nfs.svg"),
-        (places, "folder-remote-smb.svg"),
-        (places, "folder-remote-ssh.svg"),
-        (places, "folder-remote.svg"),
-        (places, "knetattach.svg"),
+    'group_name': 'Folder-likes - Remote',
+    'copies': [
+        (places, 'folder-remote-apf'),
+        (places, 'folder-remote-ftp'),
+        (places, 'folder-remote-nfs'),
+        (places, 'folder-remote-smb'),
+        (places, 'folder-remote-ssh'),
+        (places, 'folder-remote'),
+        (places, 'knetattach'),
     ]
 }
 
 network_manager_group = {
-    "group_name": "Folder-likes - Network",
-    "copies": [
-        (places, "network-manager.svg"),
-        (places, "folder-network.svg"),
-        (places, "network_local.svg"),
-        (places, "network-server.svg"),
-        (places, "network.svg"),
-        (places, "network-workgroup.svg"),
+    'group_name': 'Folder-likes - Network',
+    'copies': [
+        (places, 'network-manager'),
+        (places, 'folder-network'),
+        (places, 'network_local'),
+        (places, 'network-server'),
+        (places, 'network'),
+        (places, 'network-workgroup'),
     ]
 }
 
 user_desktop_group = {
-    "group_name": "Folder-likes - Desktop",
-    "copies": [
-        (places, "user-desktop.svg"),
-        (places, "desktop.svg"),
-        (places, "gnome-ccdesktop.svg"),
-        (places, "gnome-desktop-config.svg"),
-        (places, "gnome-desktop.svg"),
-        (places, "gnome-fs-desktop.svg"),
+    'group_name': 'Folder-likes - Desktop',
+    'copies': [
+        (places, 'user-desktop'),
+        (places, 'desktop'),
+        (places, 'gnome-ccdesktop'),
+        (places, 'gnome-desktop-config'),
+        (places, 'gnome-desktop'),
+        (places, 'gnome-fs-desktop'),
     ]
 }
--- python/track/writer.py
+++ python/track/writer.py
@@ -1,9 +1,54 @@
 from pathlib import Path
-
 from pytablewriter import MarkdownTableWriter
-
-#from contents_old import folder_like, default_folder_copies
 from contents import *
+
+def generate_table_rows(tuple_list: list[tuple]):
+    # variáveis pra futura organização
+    # assim, é possível ordenar os arquivos reais antes dos symlinks
+    real_files = []
+    symlinks = []
+
+    for t in tuple_list:
+        origin_dir = t[0] # ex: places/scalable
+        icon_name = t[1] # ex: folder-pictures.svg
+
+        # reconstruir o path de onde o ícone vem, incluindo a extensão do arquivo
+        icon_ext = icon_name + '.svg' if not icon_name.endswith('.svg') else icon_name
+        icon_path = Path('./copycat/', origin_dir, icon_ext)
+
+        # formatar os dados da entrada do dicionário de conteúdos
+        icon_image = f'<img src="{icon_path}">'
+        full_path =  f'`{icon_path}`'
+        file_name =  f'`{icon_name}`'
+        norm_origin_dir = f'`{origin_dir}`'
+        points_to = '' # é decidido por código depois
+
+        # adicionar verificação se o ícone é ou não um symlink
+        # se ele NÃO for, ganha a checkmark verde, pra indicar que é real
+        # ícones de: lucide icons
+        resolved_path = Path(__file__).resolve().parent.parent.parent / 'copycat' / origin_dir / icon_ext
+        checkmark_width = 15
+        if not resolved_path.is_symlink():
+            is_symlink = f'<img src="./python/track/check.svg" width={checkmark_width}>'
+        else:
+            is_symlink = f'<img src="./python/track/ban.svg" width={checkmark_width}>'
+
+            # deixar os textos do row em itálico caso seja um symlink
+            full_path = f'<i>{full_path}</i>'
+            file_name = f'<i>{file_name}</i>'
+            norm_origin_dir = f'<i>{norm_origin_dir}</i>'
+            points_to = f'<i>`{resolved_path.readlink()}`</i>' # ler pra onde o symlink do arquivo aponta
+
+        # adicionar mais um row aos arrays que serão condesados no array final
+        final_row = [icon_image, is_symlink, full_path, file_name, points_to or '', norm_origin_dir]
+        if resolved_path.is_symlink():
+            symlinks.append(final_row)
+        else:
+            real_files.append(final_row)
+
+    # sempre ordena os symlinks pra virem por último, apenas após todos os arquivos reais
+    rows = real_files + symlinks
+    return rows
 
 def resolve_table_writing(content_dict: dict):
     """
@@ -16,95 +61,56 @@ def resolve_table_writing(content_dict: dict):
         dicionário contendo o nome do grupo
         e um array de tuplas com o path relativo do diretório de origem e o nome do arquivo
     """
-    # obter as informações no dicionário
-    group_name = content_dict.get("group_name")
-    copies = content_dict.get("copies")
 
-    # separação de variáveis pra futura organização
-    real_files = []
-    symlinks = []
+    # obter as informações no dicionário
+    group_name = content_dict.get('group_name')
+    copies = content_dict.get('copies')
 
     # construir um row novo pra cada tupla dentro do array de cópias
-    for copy in copies:
-        origin_dir = copy[0] # ex: places/scalable
-        icon_name = copy[1] # ex: folder-pictures.svg
-
-        if not icon_name.endswith(".svg"):
-            print(f"{icon_name} é inválido por não terminar em .svgs")
-            continue
-
-        # extrair os dados da entrada do dicionário de conteúdos
-        icon_path = Path("./copycat/", origin_dir, icon_name)
-        icon_image = f'<img src="{icon_path}">'
-        full_path = f"`{icon_path}`"
-        file_name = f"`{icon_name}`"
-        norm_origin_dir = f"`{origin_dir}`"
-        points_to = "" # é definido via código depois
-
-        # adicionar verificação se o ícone é ou não um symlink
-        # se ele NÃO for, ganha a checkmark verde, pra indicar que é real
-        # ícones de: lucide icons
-        resolved_path = Path(__file__).resolve().parent.parent.parent / "copycat" / origin_dir / icon_name
-        checkmark_width = 15
-        if not resolved_path.is_symlink():
-            is_symlink = f'<img src="./python/track/check.svg" width={checkmark_width}>'
-        else:
-            is_symlink = f'<img src="./python/track/ban.svg" width={checkmark_width}>'
-
-            # deixar os textos do row em itálico caso seja um symlink
-            full_path = f"<i>{full_path}</i>"
-            file_name = f"<i>{file_name}</i>"
-            norm_origin_dir = f"<i>{norm_origin_dir}</i>"
-            points_to = f"<i>`{resolved_path.readlink()}`</i>" # ler pra onde o symlink do arquivo aponta
-
-        # adicionar mais um row aos arrays que serão condesados no array final
-        final_row = [icon_image, is_symlink, full_path, file_name, points_to, norm_origin_dir]
-        if resolved_path.is_symlink():
-            symlinks.append(final_row)
-        else:
-            real_files.append(final_row)
-
-    # sempre ordena os symlinks pra virem por último, apenas após todos os arquivos reais
-    rows = real_files + symlinks
+    rows = generate_table_rows(copies)
 
     # gerar a tabela markdown contendo os rows obtidos
     # e escrever ela como uma string com .dumps
     writer = MarkdownTableWriter(
         table_name=group_name,
-        headers=["Icon", "Is real", "Full path", "File name", "Points to", "Origin directory"],
+        headers=['Icon', 'Is real', 'Full path', 'File name', 'Points to', 'Origin directory'],
         value_matrix=rows
     )
 
     string_table = writer.dumps()
     return string_table
 
-table_plain_folder_group = resolve_table_writing(plain_folder_group)
-table_krusader = resolve_table_writing(krusader_group)
+# escrever todas as tabelas e inseri-las em um array
+# esse array vai ser condensado em uma só string pra ser escrito no arquivo final
+table_plain = resolve_table_writing(plain_folder_group)
+table_variations = resolve_table_writing(variations_group)
+table_derivations = resolve_table_writing(derivations_group)
 
-table_folder_locked = resolve_table_writing(folder_locked_group)
-table_folder_favorites = resolve_table_writing(folder_favorites_group)
-table_folder_download = resolve_table_writing(folder_download_group)
-table_folder_image = resolve_table_writing(folder_image_group)
-table_folder_remote = resolve_table_writing(folder_remote_group)
-table_network_manager = resolve_table_writing(network_manager_group)
-table_user_desktop = resolve_table_writing(user_desktop_group)
+table_locked = resolve_table_writing(folder_locked_group)
+table_favorites = resolve_table_writing(folder_favorites_group)
+table_download = resolve_table_writing(folder_download_group)
+table_image = resolve_table_writing(folder_image_group)
+table_remote = resolve_table_writing(folder_remote_group)
+table_network = resolve_table_writing(network_manager_group)
+table_desktop = resolve_table_writing(user_desktop_group)
 
 all_tables = [
-    table_plain_folder_group,
-    table_krusader,
+    table_plain,
+    table_variations,
+    table_derivations,
 
-    table_folder_locked,
-    table_folder_favorites,
-    table_folder_download,
-    table_folder_image,
-    table_folder_remote,
-    table_network_manager,
-    table_user_desktop,
+    table_locked,
+    table_favorites,
+    table_download,
+    table_image,
+    table_remote,
+    table_network,
+    table_desktop,
 ]
 
 # escrever o resultado final no arquivo markdown
-with open("../../TRACK.md", "w") as f:
-    condensed = ""
+with open('../../TRACK.md', 'w') as f:
+    condensed = ''
     for table in all_tables:
         condensed += table
     f.write(condensed)
